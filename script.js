8314b988e4d0dc6f85909054b4c990b4df312d56
drehbuch <tehninjah@me.com>
2017-04-04 13:29:59 +0100

    Add site

---
+++ script.js
@@ -0,0 +1,20 @@
+function quoteHide() {
+  document.getElementById('largequote').style.visibility = 'hidden';
+}
+
+function quoteShow(quoteid) {
+  var selectSpeaker = document.getElementById(quoteid);
+  var selectText = selectSpeaker.parentElement.getElementsByTagName("td")[0];
+  
+  var large = document.getElementById('largequote')
+  var largeSpeaker = large.getElementsByTagName('h1')[0]
+  var largeText = large.getElementsByTagName('p')[0]
+  
+  largeSpeaker.innerHTML = selectSpeaker.innerHTML;
+  largeText.innerHTML = selectText.innerHTML;
+  large.style.visibility = 'visible';
+  
+  /* Size text based on length */
+  height = 150 / Math.log(selectText.innerHTML.length / 5);
+  largeText.style.fontSize = height + "px";
+}

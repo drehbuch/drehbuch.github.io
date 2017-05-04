4a121db3d0a87128c1f96dab597599bb368ee24a
Jack Neil Dobson <jackndobson@gmail.com>
2017-05-04 17:32:19 +0100

    Make large quote have stained glass effect
    Because, y'know, it doesn't fit the whole typewritery vibe... but it
    LOOKS cool :)

--- quote.js
+++ quote.js
@@ -1,25 +1,29 @@
-var currentSpeaker, currentQuote;
-
-function largeQuoteHide() {
-  document.getElementById('largeQuote').style.visibility = 'hidden';
-}
-
-function largeQuoteShow() {  
-  var large = document.getElementById('largeQuote');
-  var largeSpeaker = large.getElementsByTagName('h1')[0];
-  var largeText = large.getElementsByTagName('p')[0];
-  
-  largeSpeaker.innerHTML = currentSpeaker.innerHTML;
-  largeText.innerHTML = currentQuote.innerHTML;
-  large.style.visibility = 'visible';
-  
-  /* Size text based on length */
-  height = 150 / Math.log(currentQuote.innerHTML.length / 5);
-  largeText.style.fontSize = height + "px";
-}
+var currentSpeaker, currentQuote, contentBox, largeQuote;
 
 function quoteClick(selectSpeaker) {
   currentSpeaker = selectSpeaker;
   currentQuote = currentSpeaker.parentElement.getElementsByTagName("td")[0];
+  contentBox = document.getElementById('content');
+  largeQuote = document.getElementById('largeQuote')
+  
   largeQuoteShow();
+}
+
+function largeQuoteHide() {
+  largeQuote.style.visibility = 'hidden';
+  contentBox.classList.remove('blurred')
+}
+
+function largeQuoteShow() {
+  contentBox.classList.add('blurred')
+  var largeSpeaker = largeQuote.getElementsByTagName('h1')[0];
+  var largeText = largeQuote.getElementsByTagName('p')[0];
+  
+  largeSpeaker.innerHTML = currentSpeaker.innerHTML;
+  largeText.innerHTML = currentQuote.innerHTML;
+  largeQuote.style.visibility = 'visible';
+  
+  /* Size text based on length */
+  height = 150 / Math.log(currentQuote.innerHTML.length / 5);
+  largeText.style.fontSize = height + "px";
 }

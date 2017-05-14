8c10e9ad833eebad1dd21232bb525750f425e53c
Jack Neil Dobson <jackndobson@gmail.com>
2017-05-14 01:56:27 +0100

    Overhaul line system
    We've set up the groundwork for a lot of line-based tweaks, changes etc.

--- quote.js
+++ quote.js
@@ -1,12 +1,37 @@
-var currentSpeaker, currentQuote, contentBox, largeQuote;
+var contentBox, largeQuote, lines = [], currentLine;
 
-function quoteClick(selectSpeaker) {
-  currentSpeaker = selectSpeaker;
-  currentQuote = currentSpeaker.parentElement.getElementsByTagName("td")[0];
+window.onload = function(){
   contentBox = document.getElementById('content');
-  largeQuote = document.getElementById('largeQuote')
+  largeQuote = document.getElementById('largeQuote');
   
-  largeQuoteShow();
+  var rows = document.getElementsByTagName("tr")
+  for( i = 0; i < rows.length; i++ ){
+    var line = new Line(rows[i]);
+    if( line.isQuote ){
+      lines.push(line);
+      line.th.onclick = largeQuoteShow;
+      line.th.onmouseover = quoteOver;
+    }    
+  }
+}
+
+function Line(tr) {
+  this.tr = tr;
+  this.td = tr.getElementsByTagName("td")[0];
+  this.th = tr.getElementsByTagName("th")[0];
+  this.isQuote = Boolean(this.td)
+  if( this.td ){
+    this.speaker = this.th.innerText;
+    this.quote = this.td.innerText;
+  } else {
+    this.speaker = "[Direction]"
+    this.quote = this.th.innerText;
+  }
+}
+
+
+function quoteOver() {
+  currentLine = new Line(this.parentElement);
 }
 
 function largeQuoteHide() {
@@ -19,11 +44,11 @@ function largeQuoteShow() {
   var largeSpeaker = largeQuote.getElementsByTagName('h1')[0];
   var largeText = largeQuote.getElementsByTagName('p')[0];
   
-  largeSpeaker.innerHTML = currentSpeaker.innerHTML;
-  largeText.innerHTML = currentQuote.innerHTML;
+  largeSpeaker.innerHTML = currentLine.speaker;
+  largeText.innerHTML = currentLine.td.innerHTML;
   largeQuote.style.visibility = 'visible';
   
   /* Size text based on length */
-  height = 150 / Math.log(currentQuote.innerHTML.length / 5);
+  height = 150 / Math.log(largeText.innerHTML.length / 5);
   largeText.style.fontSize = height + "px";
 }

4c1561105b92156c4691b55a2e4092ad4d9fe759
Jack Neil Dobson <jackndobson@gmail.com>
2017-05-18 18:39:42 +0100

    Organise JS
    Man, it'd be nicer if Git recognised *movement* of blocks rather than
    just 'delete + add'. I want my code to be organised, and the fear of
    change means I'm sure some keep it unorganised to avoid any merge
    conflicts.

--- interface.js
+++ interface.js
@@ -1,3 +1,41 @@
+var contentBox, largeQuote, lineIndexes = [];
+
+window.onload = function(){
+  /* initialise variables and show interaction elements */
+  contentBox = document.getElementById('content');
+  largeQuote = document.getElementById('largeQuote');
+  
+  document.getElementById('instructions').classList.remove('hidden');
+  document.getElementById('searchbox').classList.remove('hidden');
+  
+  /* Show theming if CSS variables work */
+  if( window.CSS && window.CSS.supports && window.CSS.supports('--variable', 0) ){
+    document.getElementById('themes').classList.remove('hidden');
+  }
+  /* Signal to CSS that we have JS (for onclick hinting) */
+  contentBox.classList.add('js')
+  
+  /* Gather index and assign onclicks */
+  var rows = document.getElementsByTagName("tr")
+  for( i = 0; i < rows.length; i++ ){
+    var line = new Line(rows[i]);
+    lineIndexes.push(line);
+    if( line.isQuote ){
+      line.th.onclick = showSpeaker;
+      line.td.onclick = largeQuoteShow;
+    } else {
+      line.th.onclick = largeQuoteShow;
+    }
+  }
+}
+
+/* Basic style functionality */
+
+function changeTheme(themeIdentifier) {
+  var styler = document.getElementById('styler');
+  styler.href = 'themes/' + theme + '.css';
+}
+
 document.onscroll = window.onresize = function() {
   var nav = document.getElementById('contents'),
       background = document.getElementById('background'),
@@ -17,11 +55,28 @@ document.onscroll = window.onresize = function() {
   }
 }
 
-String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
-
-function changeTheme(theme) {
-  var styler = document.getElementById('styler');
-  styler.href = 'themes/' + theme + '.css';
+function quoteHighlight(selector) {
+  switch( typeof selector ){
+    case "function":
+      condition = selector;
+      break;
+    case "boolean":
+      condition = function(line){ return selector };
+      break;
+    default:
+      condition = function(line){ return false };
+      break;
+  }
+  
+  var firstLine;
+  for( i = 0; i < lines.length; i++ ){
+    var line = lineIndexes[i], c = line.tr.classList;
+    if( condition(line) ){
+      c.add('highlighted')
+      if( firstLine == undefined ){ firstLine = line; }
+    } else {
+      c.remove('highlighted')
+    }
+  }
+  return firstLine;
 }
--- quote.js
+++ quote.js
@@ -1,36 +1,23 @@
-var contentBox, largeQuote, lines = [];
+/* String and quote-based helper functions. */
 
-window.onload = function(){
-  /* initialise variables and show interaction elements */
-  contentBox = document.getElementById('content');
-  largeQuote = document.getElementById('largeQuote');
-  
-  document.getElementById('instructions').classList.remove('hidden');
-  document.getElementById('searchbox').classList.remove('hidden');
-  
-  /* Show theming if CSS variables work */
-  if( window.CSS && window.CSS.supports && window.CSS.supports('--variable', 0) ){
-    document.getElementById('themes').classList.remove('hidden');
-  }
-  /* Signal to CSS that we have JS (for onclick hinting) */
-  contentBox.classList.add('js')
-  
-  /* Gather rows and assign onclicks */
-  var rows = document.getElementsByTagName("tr")
-  for( i = 0; i < rows.length; i++ ){
-    var line = new Line(rows[i]);
-    if( line.isQuote ){
-      lines.push(line);
-      line.td.onclick = largeQuoteShow;
-      line.th.onclick = thOnclick;
-    }    
-  }
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+/* Makes a string 'search equivalent' for German. */
+neutralise = function(string){
+  return string.toLowerCase()
+         .replace(new RegExp('ä', 'g'), "a")
+         .replace(new RegExp('ö', 'g'), "o")
+         .replace(new RegExp('ü', 'g'), "u")
+         .replace(new RegExp('ß', 'g'), "s");
 }
 
-thOnclick = function(/* th */){
-  var speaker = this.parentElement.classList.contains("highlighted")
-    ? undefined : this.innerText
-  quoteHighlight(speaker);
+scrollIntoView = function(element) {
+  element.scrollIntoView({block: "end", behavior: "smooth"});
+    /* Non-Gecko engines interpret these settings as 'true' == pin to top,
+     * so we correct it by immediately scrolling down. */
+  element.scrollIntoView(false);
 }
 
 function Line(tr) {
@@ -45,48 +32,16 @@ function Line(tr) {
     this.speaker = "[Direction]"
     this.quote = this.th.innerText;
   }
-  this.quote = neutralise(this.quote)
-}
-
-function neutralise(string){
-  return string.toLowerCase()
-         .replace(new RegExp('ä', 'g'), "a")
-         .replace(new RegExp('ö', 'g'), "o")
-         .replace(new RegExp('ü', 'g'), "u")
-         .replace(new RegExp('ß', 'g'), "s");
-}
-
-function findQuote() {
-  var request = neutralise(document.getElementById('searchbar').value);
-  
-  if( request.length <= 1 ){
-    /* too many returned values, most likely */
-    quoteHighlight();
-    return;
-  }
-  
-  var firstFound = quoteHighlight(function(line){
-    return line.quote.indexOf(request) !== -1
-  })
-  
-  if( firstFound ){
-    firstFound.tr.scrollIntoView({block: "end", behavior: "smooth"});
-    /* Non-Gecko engines interpret these settings as 'true' == pin to top,
-     * so we correct it by immediately scrolling down. */
-    firstFound.tr.scrollIntoView(false);
-    }
+  this.searchableQuote = neutralise(this.quote)
 }
 
 function quoteHighlight(selector) {
   switch( typeof selector ){
-    case "string":
-      condition = function(line){ return selector == line.speaker };
+    case "function":
+      condition = selector;
       break;
     case "boolean":
       condition = function(line){ return selector };
-      break;
-    case "function":
-      condition = selector;
       break;
     default:
       condition = function(line){ return false };
@@ -94,8 +49,8 @@ function quoteHighlight(selector) {
   }
   
   var firstLine;
-  for( i = 0; i < lines.length; i++ ){
-    var line = lines[i], c = line.tr.classList;
+  for( i = 0; i < lineIndexes.length; i++ ){
+    var line = lineIndexes[i], c = line.tr.classList;
     if( condition(line) ){
       c.add('highlighted')
       if( firstLine == undefined ){ firstLine = line; }
@@ -105,6 +60,8 @@ function quoteHighlight(selector) {
   }
   return firstLine;
 }
+
+/* Quote interaction functionality */
 
 function largeQuoteHide() {
   largeQuote.classList.add('hidden');
@@ -124,4 +81,30 @@ function largeQuoteShow(/* td */) {
   /* Size text based on length */
   height = 150 / Math.log(largeText.innerHTML.length / 5);
   largeText.style.fontSize = height + "px";
+}
+
+function showSpeaker(/* th */){
+  if( this.parentElement.classList.contains("highlighted") ){
+    quoteHighlight( function(line){
+      return line.speaker == this.innerText
+    })
+  } else {
+    quoteHighlight()
+  }
+}
+
+function findQuote() {
+  var request = neutralise(document.getElementById('searchbar').value);
+  
+  /* likely too many results, so simply do nothing */
+  if( request.length <= 2 ){ return; }
+  
+  var firstFound = quoteHighlight(function(line){
+    return line.searchableQuote.indexOf(request) !== -1
+  })
+  
+  if( firstFound ){
+    scrollIntoView(firstFound.tr)
+  }
+  return false;
 }
